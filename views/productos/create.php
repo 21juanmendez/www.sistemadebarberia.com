928f59cbc7e6986ba459025344102b180640d1c4
obed011 <158543664+obed011@users.noreply.github.com>
2024-05-31 16:26:07 -0600

    frontend de Registrar producto

--- views/productos/create.php
+++ views/productos/create.php
@@ -1,7 +1,113 @@
 <?php
 include('../usuarios/layout/parte1.php');
 ?>
+<!--codigo html-->
+<div class="container-fluid">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title"><b>CREAR NUEVO PRODUCTO</b></h3>
+        </div>
+        <div class="card-body">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="row">
 
+                            <input name="id_usuario" type="text" class="form-control" value="" hidden>
+
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <label>Codigo</label>
+                                    <input value="" type="text" class="form-control" disabled>
+                                    <input hidden value="" type="text" name="codigo" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div class="form-group">
+                                    <label>Nombre del producto</label>
+                                    <input type="text" name="nombre_producto" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label>Fecha de ingreso</label>
+                                    <input type="date" name="fecha_de_ingreso" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label>Descripcion</label>
+                                    <textarea name="descripcion" class="form-control" rows="1"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Categoria</label>
+                                    <select name="id_categoria" id="" class="form-control" required>
+                                            <option value="">Seleccione una categoria</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <label>Stock</label>
+                                    <input type="number" name="stock" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <label>Stock minimo</label>
+                                    <input type="number" name="stock_minimo" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <label>Stock maximo</label>
+                                    <input type=number name="stock_maximo" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label>Precio de compra</label>
+                                    <input type="number" name="precio_compra" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label>Precio de venta</label>
+                                    <input type="number" name="precio_venta" class="form-control" required>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Seleccione la imagen del producto</label>
+                            <input name="file" class="form-control" type="file" id="file" multiple>
+                            <center>
+                                <output id="list">
+
+                                </output>
+                            </center>
+                        </div>
+                    </div>
+
+                </div>
+                <center>
+                    <button class="btn btn-primary" type="submit">Registrar</button>
+                    <a href="###" class="btn btn-secondary">Cancelar</a>
+                </center>
+
+            </form>
+        </div>
+    </div>
+</div>
 <?php
 include('../usuarios/layout/parte2.php');
 ?>
