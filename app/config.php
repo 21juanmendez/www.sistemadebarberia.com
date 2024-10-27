08d53659f859b0da50b47fd5e981c887c7e84307
Stephy10 <astephyce@gmail.com>
2024-10-26 19:38:07 -0600

    Cambios en archivo config.php

--- app/config.php
+++ app/config.php
@@ -2,15 +2,15 @@
 // Nombre de la aplicación
 define('APP_NAME', 'Sistema de Barberia');
 
-// Obtiene las credenciales de conexión desde las variables de entorno
-$servidor = "mysql:dbname=" . getenv('DB_DATABASE') . ";host=" . getenv('DB_HOST');
-$usuario = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+// Obtiene las credenciales de conexión desde las variables de entorno de Clever Cloud
+$servidor = "mysql:dbname=" . getenv('MYSQL_ADDON_DB') . ";host=" . getenv('MYSQL_ADDON_HOST') . ";port=" . getenv('MYSQL_ADDON_PORT');
+$usuario = getenv('MYSQL_ADDON_USER');
+$password = getenv('MYSQL_ADDON_PASSWORD');
 
 // Intenta conectar con la base de datos usando PDO
 try {
     $pdo = new PDO($servidor, $usuario, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    // echo "Conexion Exitosa"; // Puedes descomentar para probar la conexión
+    // echo "Conexion Exitosa"; // Puedes descomentar esta línea para probar la conexión
 } catch (PDOException $e) {
     // En caso de error de conexión, imprime el mensaje de error
     echo "Error de conexión a la base de datos: " . $e->getMessage();
@@ -25,4 +25,4 @@ $fyh_actualizacion = date('Y-m-d H:i:s');
 // URLs base para el proyecto
 $URL = "http://localhost/www.sistemadebarberia.com";
 $VIEWS = "http://localhost/www.sistemadebarberia.com/views";
-
+?>
