092d17b725deea1c96a932385eef2220d1e96aa4
Stephy10 <astephyce@gmail.com>
2024-10-26 20:12:19 -0600

    Cambios en archivo config

--- app/config.php
+++ app/config.php
@@ -2,14 +2,12 @@
 // Nombre de la aplicación
 define('APP_NAME', 'Sistema de Barberia');
 
-// Obtiene las credenciales de conexión desde las variables de entorno de Clever Cloud
-$servidor = "mysql:dbname=" . getenv('MYSQL_ADDON_DB') . ";host=" . getenv('MYSQL_ADDON_HOST') . ";port=" . getenv('MYSQL_ADDON_PORT');
-$usuario = getenv('MYSQL_ADDON_USER');
-$password = getenv('MYSQL_ADDON_PASSWORD');
+// Obtiene el URI de conexión directamente desde la variable de entorno
+$connectionUri = getenv('MYSQL_ADDON_URI');
 
 // Intenta conectar con la base de datos usando PDO
 try {
-    $pdo = new PDO($servidor, $usuario, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $pdo = new PDO($connectionUri, null, null, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     // echo "Conexion Exitosa"; // Puedes descomentar esta línea para probar la conexión
 } catch (PDOException $e) {
     // En caso de error de conexión, imprime el mensaje de error
@@ -26,3 +24,4 @@ $fyh_actualizacion = date('Y-m-d H:i:s');
 $URL = "http://localhost/www.sistemadebarberia.com";
 $VIEWS = "http://localhost/www.sistemadebarberia.com/views";
 ?>
+
